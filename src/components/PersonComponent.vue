d40e9ddfafca623970d2f7ddb54e9033cad4f8bf
sabbasachiPH <rentustat02@gmail.comm>
2024-02-17 16:52:13 +0600

    object pass complete

---
+++ src/components/PersonComponent.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+const props = defineProps(['person']);
+
+</script>
+
+<template>
+    <h3 class="text-3xl ">Child Components</h3>
+    <div>
+        <p>
+            <label>Name - {{ person.name }}</label> <br />
+            <input v-model="person.name" class="mx-auto p-5 m-5 text-xl text-black" type="text">
+        </p>
+        <p>
+            <label>Email - {{ person.email }}</label> <br />
+            <input v-model="person.email" class="mx-auto p-5 m-5 text-xl text-black" type="text">
+        </p>
+    </div>
+</template>
+
+
+
+<style scoped></style>
